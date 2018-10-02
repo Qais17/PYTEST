ae91bc43afd81a3f3dc7921e10ad93ca7aaf513d
BelKais <qaisbewebpro@gmail.com>
2018-10-02 21:49:35 +0200

    Modularité:
    - Fonction
    - Module
    - Package

---
+++ multitest/multipli.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3.5
+# -*-coding:Utf-8 -*
+
+
+import os
+
+def table(nb, max=10):
+    i = 0
+    while i < max:
+        print(i + 1, "*", nb, "=", (i + 1) * nb)
+        i += 1
+
+if __name__ == "__main__":
+    table(4)
+    os.system("pause")
---
+++ multitest/test.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3.5
+# -*-coding:Utf-8 -*
+
+import os
+from multipli import *
+
+# test de la fonction table
+table(3, 20)
+os.system("pause")
