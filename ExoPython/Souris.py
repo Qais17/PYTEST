0a0cbe9b899216936c4732a2b04a88369e781a56
BelKais <qaisbewebpro@gmail.com>
2018-10-05 23:06:35 +0200

    Ajout Exo Soury

---
+++ ExoPython/Souris.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3.5
+# -*-coding:Utf-8 -*
+
+pt = 8
+
+print(" Vous achetez un couple de souris.Une souris produit une moyenne de 8 souriceaux par portée.La gestation dure 3 semaines.Vous voulez maintenir votre population à 550 individus. Au bout de combien de temps, vous alarmez-vous ? ")
+
+i = 0
+
+while i < 69:
+
+	print(" 3 Semaines se sont ecoulee vous etes a ",(i + 1) * pt, "souris.")
+
+	i += 1
+
+print(" Au bout de ", 68 * 3, " semaines vous avez atteind la limite. ")
+		
+
+
+
