576cb923731a74769bd90cdf13891a3c9183d573
BelKais <qaisbewebpro@gmail.com>
2018-10-09 23:44:56 +0200

    OBJ:
    Formatage + concatenation

---
+++ ExoPython/Formatage.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3.5
+# -*-coding:Utf-8 -*
+
+dev = """
+{prenom}, {nom}
+ {poste} {employeur} ({pays})
+""".format(prenom="Paul", nom="Duan", poste="developpeur", employeur="Pole Emploi", pays="France")
+print(dev)
---
+++ ExoPython/conca_t_ner.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3.5
+# -*-coding:Utf-8 -*
+
+prenom = "Morgan"
+Hello = "Good morning"
+message = "today is monday March 15, 2032"
+
+chaine_complete = Hello + " " + prenom + " " + message
+
+print(chaine_complete)
