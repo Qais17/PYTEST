a82f9fa2eabc75558bd4d90927c83ae183ca1760
BelKais <qaisbewebpro@gmail.com>
2018-10-13 18:26:00 +0200

    Les_Liste

---
+++ ExoPython/DListes&DTuples.py/blackList.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3.5
+# -*-coding:Utf-8 -
+
+black_list = ['toujours actif','au rendez vous', 'inactif','au commissariat']
+for i, elt in enumerate(black_list):
+	print("la cible {} est {}.".format(i, elt))
+
---
+++ ExoPython/DListes&DTuples.py/whiteList.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3.5
+# -*-coding:Utf-8 -
+
+white_list = [
+     [1, 'U'],
+     [2, 's'],
+     [3, 'a'],
+     [4, 'i'],
+     [5, 'n'],
+     [6, 'B'],
+     [7, 'o'],
+     [8, 'l'],
+     [9, 't'],
+]
+for nb, lettre in white_list:
+     print("Le coureur {} est a la {}eme place.".format(lettre, nb)) 
+
